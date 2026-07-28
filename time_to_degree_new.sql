bb6a79a0d7943b0d809d1f195eaa0bdf96d9caa0
dekockjt <justin.dekock@slu.edu>
2026-07-28 10:28:35 -0500

    updated time to degree/legacy populations

--- time_to_degree_new.sql
+++ time_to_degree_new.sql
@@ -1,3 +1,4 @@
+-- think this is working as of 7/28/2026å
 -- calculate time to degree for legacy purposes
 -- assume fall start if it's a summer
 select * from (
@@ -11,10 +12,9 @@ with term as (
         a.sgbstdn_degc_code_1 as degree_code,
         a.sgbstdn_program_1 as prog_code,
         m.stvmajr_cipc_code as cipc_code,
-        (   -- first term not matching legacy codes OR first term showing transfer
+        (   -- mininum term in current academic study
             -- UG changing to different bachelor degree doesn't constitute change. Example BS-BA is considered same
             -- minimum term less than current sgbstdn term, > max closed term, >= max new student term
-            -- a is current term, z is min term
             select min(z.sgbstdn_term_code_eff)
             from sgbstdn z
             join stvmajr zm on zm.stvmajr_code = z.sgbstdn_majr_code_1
@@ -32,7 +32,7 @@ with term as (
                     and zm.stvmajr_cipc_code = m.stvmajr_cipc_code
                 )
             )
-            -- min term >= max term with new student type
+            -- min term >= max term with new student type not matching current program
             and z.sgbstdn_term_code_eff >= nvl((
                 select max(y.sgbstdn_term_code_eff)
                 from sgbstdn y
@@ -61,26 +61,6 @@ with term as (
                 where y.sgbstdn_pidm = a.sgbstdn_pidm
                 and y.sgbstdn_term_code_eff <= a.sgbstdn_term_code_eff
                 and y.sgbstdn_stst_code in (select stvstst_code from stvstst where stvstst_reg_ind = 'N')
-            --     and (
-            --     (
-            --         a.sgbstdn_levl_code = 'UG'
-            --         and y.sgbstdn_levl_code = a.sgbstdn_levl_code
-            --         and (
-            --             (a.sgbstdn_degc_code_1 like 'B%' and y.sgbstdn_degc_code_1 like 'B%')
-            --             or (a.sgbstdn_degc_code_1 = 'CERT' and y.sgbstdn_degc_code_1 = 'CERT')
-            --         )
-            --     ) or (
-            --         a.sgbstdn_levl_code <> 'UG'
-            --         and (
-            --             zm.stvmajr_cipc_code = m.stvmajr_cipc_code
-            --             or (
-            --                 y.sgbstdn_majr_code_1 = a.sgbstdn_majr_code_1
-            --                 and y.sgbstdn_degc_code_1 = a.sgbstdn_degc_code_1
-            --             )
-            --         )
-                    
-            --     )
-            -- )
             ), '000000')
         ) as first_term
     from sgbstdn a
@@ -93,7 +73,6 @@ with term as (
         select max(z.sgbstdn_term_code_eff)
         from sgbstdn z
         where z.sgbstdn_term_code_eff <= (select t from term)
-        and z.sgbstdn_levl_code = a.sgbstdn_levl_code
         and z.sgbstdn_pidm = a.sgbstdn_pidm
     )
 ), hours as (
@@ -116,7 +95,7 @@ with term as (
 )
 select
     a.pidm,
-    s.spriden_id as student_id,
+    s.spriden_id as bid,
     a.student_level,
     a.major_code,
     a.degree_code, 
@@ -127,26 +106,23 @@ select
 from stu a
 join hours b on b.pidm = a.pidm
 join spriden s on s.spriden_pidm = a.pidm and s.spriden_change_ind is null
--- where exists (
---     select 1 from awards r
---     where r.pidm = a.pidm
---     and r.term between a.first_term and (select t from term)
--- )
+where exists (
+    select 1 from awards r
+    where r.pidm = a.pidm
+    and r.term between a.first_term and (select t from term)
 )
---  where student_id = '000074895'
--- where student_level = 'PL'
--- and student_id = '000714023'/
--- and terms_enrolled = 3
--- and spriden_id = '000416971' 
+)
 order by first_term
 -- )
 ;
 
+-- 201320 before changes, expect 201710 after
+select robnyud_pidm, robnyud_value_194, robnyud_value_195
+from robnyud 
+where robnyud_value_194 is not null
+or robnyud_value_195 is not null
+order by robnyud_value_194 ;
 
-select * from stvstst where stvstst_code like 'N%';
-select * from stvstst where stvstst_desc like '%Closed%';
-select stvstst_code from stvstst where stvstst_reg_ind = 'Y';
+update robnyud set robnyud_value_194 = null, robnyud_value_195 = null
+where robnyud_value_194 is not null or robnyud_value_195 is not null;
 
-select * from stvstst where stvstst_code like 'C%';
-select * from stvdegc where stvdegc_code like 'C%';
-select * from sgbstdn where sgbstdn_term_code_eff = '202620';
